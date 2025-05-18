59a5a3af859f5e4e2cb35f1863e477f4616802fb
testgcahm <75790323+Mahmadabid@users.noreply.github.com>
2025-05-18 13:48:51 +0500

    app final

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,12 +1,21 @@
 'use client';
 
+import { useIsOnline } from "@/components/IsOnlineContext";
 import SettingsForm from "@/components/settings/SettingsForm";
-import { ArrowUturnLeftIcon } from "@heroicons/react/16/solid";
+import { ArrowUturnLeftIcon, ExclamationTriangleIcon } from "@heroicons/react/16/solid";
 
 export default function SettingsPage() {
 
+  const isOnline = useIsOnline();
+
   return (
     <div className="flex flex-col items-center px-2 mt-5 min-h-[30vh]">
+      {!isOnline && (
+        <div className="flex justify-center items-center mx-2 text-green-700 bg-green-100 border border-green-300 rounded p-2 mb-6 font-semibold">
+          <ExclamationTriangleIcon className="w-6 h-6 text-green-700 mr-2" />
+          You're offline. Settings will still work in Offline Mode.
+        </div>
+      )}
       {/* Go back button */}
       <div className="flex items-center mb-4">
         <button
@@ -16,11 +25,11 @@ export default function SettingsPage() {
           <ArrowUturnLeftIcon className="w-5 h-5" /> Back
         </button>
       </div>
-        <h1 className="text-2xl text-secondary font-bold mb-4">Settings</h1>
-        <>
-          <p className="text-foreground mb-4">Define the start and end dates for each quarter:</p>
-          <SettingsForm />
-        </>
-      </div>
-      );
+      <h1 className="text-2xl text-secondary font-bold mb-4">Settings</h1>
+      <>
+        <p className="text-foreground mb-4">Define the start and end dates for each quarter:</p>
+        <SettingsForm />
+      </>
+    </div>
+  );
 }
