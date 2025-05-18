45cefa9f28daa9b78687133d551343d23d392fac
testgcahm <75790323+Mahmadabid@users.noreply.github.com>
2025-05-18 23:16:49 +0500

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 // public/sw.js
-const STATIC_CACHE = 'static-cache-v1';
-const DATA_CACHE = 'data-cache-v1';
+const STATIC_CACHE = 'static-cache-v3';
+const DATA_CACHE = 'data-cache-v3';
 const FETCH_URL = '/api/dummy';
 
 // List all the URLs you need for offline — pages + public assets
